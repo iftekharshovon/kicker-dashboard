f9932c86f6a2c6e257d9794b7c156df19ee41df2
Iftekhar <iftekharshovonseu17@gmail.com>
2024-06-22 15:16:55 +0600

    update: draawer responsive

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.getElementById("menuButton");
   const closeDrawerButton = document.getElementById("closeDrawerButton");
   const drawer = document.getElementById("drawer");
+  const drawerItems = document.querySelectorAll("#drawer a");
 
   menuButton.addEventListener("click", function () {
     drawer.classList.remove("drawer-hidden");
@@ -12,4 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
     drawer.classList.remove("drawer-visible");
     drawer.classList.add("drawer-hidden");
   });
+  function checkScreenSize() {
+    if (window.innerWidth >= 1024) {
+      drawer.classList.add("hidden");
+    }
+  }
+
+  closeDrawerButton.addEventListener("click", () => {
+    drawer.classList.add("hidden");
+  });
+
+  drawerItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (window.innerWidth < 1024) {
+        drawer.classList.add("hidden");
+      }
+    });
+  });
+
+  window.addEventListener("resize", checkScreenSize);
+
+  // Initial check
+  checkScreenSize();
 });
